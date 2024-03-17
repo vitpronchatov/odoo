68b26c28a41ac488acabe894ab175dcfebdd1304
vitpronchatov <70130118+vitpronchatov@users.noreply.github.com>
2024-03-17 23:30:51 +0200

    Delete odoo_telegram_client directory

--- odoo_telegram_client/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import telegram_client
-from . import extend_res_user
-from . import extend_mail_message
-from . import extend_res_partners
-from . import extend_mail_channel
-
--- odoo_telegram_client/models/extend_res_user.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.users'
-
-    telegram_client_id = fields.Many2one('telegram.client', string='Telegram Client Account')
